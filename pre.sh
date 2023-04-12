c6bd3e0a69829a56f217fa3163df4308744fb1f5
Charlie May <cmay5@wisc.edu>
2023-04-12 17:10:27 -0500

    updated pre

--- pre.sh
+++ pre.sh
@@ -2,8 +2,8 @@
 
 wget https://pages.stat.wisc.edu/~jgillett/DSCP/CHTC/wordCounting/shakespeare.tar
 
-tar xf shakespeare.tar
+tar xvf shakespeare.tar
 
-cat shakespeare/*.txt > all_plays.txt
+cat $(find shakespeare -type f) > all_plays.txt
 
 split -l $(( $(wc -l < all_plays.txt) / 5 + 1 )) all_plays.txt chunk_
