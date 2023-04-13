018eb8d1e9dee8782c8c8c035d80b54eeceb60fe
Charlie May <cmay5@wisc.edu>
2023-04-13 12:35:15 -0500

    updated post.sh which counts words and sorts

--- post.sh
+++ post.sh
@@ -2,9 +2,9 @@
 
 
 export LC_ALL = C 
-sort -m formatted_chunk_* -o merge_file.txt
+sort -m chunk_* -o merge_file.txt
 
-uniq -c merged_file.txt > countsOfWords
+uniq -c merge_file.txt > countsOfWords
 sort -nr -o countsOfWords countsOfWords
 
 echo "countsOfWords"
