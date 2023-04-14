3768003adde4342a0c70d9f9c793fdf94df0b84f
Brandon <90635331+b4wolf@users.noreply.github.com>
2023-04-14 14:45:51 -0500

    Update process.sh

--- process.sh
+++ process.sh
@@ -1,8 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 
-for i in chunk_aa chunk_ab chunk_ac chunk_ad chunk_ae: do tr '[:upper:]' '[:lower:]' | sed -e 's/[[:punct:]]//g' | sed -e 's/[\t]/\n/g' | grep -v "^\s*$" | sort; done
+chunks=("chunk_aa" "chunk_ab" "chunk_ac" "chunk_ad" "chunk_ae")
+for chunk in "${chunks[@]}"; do
+  cat "${chunk}" | tr '[:upper:]' '[:lower:]' | sed -e 's/[[:punct:]]//g' | sed -e 's/[[:space:]]/\n/g' | grep -v "^[[:space:]]*$" | sort > "${chunk}_sorted.txt"
+done
 
 
-
+#for i in chunk_aa chunk_ab chunk_ac chunk_ad chunk_ae: do tr '[:upper:]' '[:lower:]' | sed -e 's/[[:punct:]]//g' | sed -e 's/[\t]/\n/g' | grep -v "^\s*$" | sort; done
 # cat chunk_aa | tr '[:upper:]' '[:lower:]' | sed -e 's/[[:punct:]]//g' | sed -e 's/[[:space:]]/\n/g' | grep -v "^[[:space:]]*$" | sort > "chunk_aa_sorted.txt"
 
