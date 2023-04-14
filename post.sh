a4191cc845bb1748cfdded95dc4eabeafe41db14
Brandon <90635331+b4wolf@users.noreply.github.com>
2023-04-14 14:47:28 -0500

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,19 +1,14 @@
 #! /bin/bash
 
-
-export LC_ALL = C 
-sort -m chunk_* -o merge_file.txt
-
-uniq -c merge_file.txt > countsOfWords
-sort -nr -o countsOfWords countsOfWords
-
-echo "countsOfWords"
+export LC_ALL=C
+sort -m -o merged_sorted.txt *_sorted.txt
+uniq -c merged_sorted.txt | sort -rn > countsOfWords.txt
 
 
+#V1   changed to make sure it works with the updated version of process. 
+#export LC_ALL = C 
+#sort -m chunk_* -o merge_file.txt
+#uniq -c merge_file.txt > countsOfWords
+#sort -nr -o countsOfWords countsOfWords
+#echo "countsOfWords"
 
-# Merge the sorted small files into one large sorted file
-#export LC_ALL=C
-#sort -m -o merged_sorted.txt *_sorted.txt
-
-# Convert the merged sorted file to countsOfWords and sort by descending count
-#uniq -c merged_sorted.txt | sort -rn > countsOfWords.txt
