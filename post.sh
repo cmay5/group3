9a0f8cb84843e52eb8bb125f75d0468a36ebc134
Brandon <90635331+b4wolf@users.noreply.github.com>
2023-04-14 11:52:35 -0500

    Update post.sh

--- post.sh
+++ post.sh
@@ -8,3 +8,12 @@ uniq -c merge_file.txt > countsOfWords
 sort -nr -o countsOfWords countsOfWords
 
 echo "countsOfWords"
+
+
+
+# Merge the sorted small files into one large sorted file
+#export LC_ALL=C
+#sort -m -o merged_sorted.txt *_sorted.txt
+
+# Convert the merged sorted file to countsOfWords and sort by descending count
+#uniq -c merged_sorted.txt | sort -rn > countsOfWords.txt
