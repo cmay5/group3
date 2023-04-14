f52b7d6ae2f4146c1e5e0e4c7bc614a141341829
Brandon <90635331+b4wolf@users.noreply.github.com>
2023-04-14 11:50:40 -0500

    Update process.sh

--- process.sh
+++ process.sh
@@ -4,5 +4,5 @@ for i in chunk_aa chunk_ab chunk_ac chunk_ad chunk_ae: do tr '[:upper:]' '[:lowe
 
 
 
-
+# cat chunk_aa | tr '[:upper:]' '[:lower:]' | sed -e 's/[[:punct:]]//g' | sed -e 's/[[:space:]]/\n/g' | grep -v "^[[:space:]]*$" | sort > "chunk_aa_sorted.txt"
 
